ea76523fc104fe6652dafa6b0d50770b27fe6e2a
Andrew Stakhov <andrew.stakhov@gmail.com>
2021-02-11 13:10:37 -0500

    Build fix

--- src/FunnyQuotesUIForms/Clients/WcfFunnyQuotesClient.cs
+++ src/FunnyQuotesUIForms/Clients/WcfFunnyQuotesClient.cs
@@ -2,6 +2,7 @@
 using System.ServiceModel;
 using System.Threading.Tasks;
 using FunnyQuotesCommon;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Steeltoe.CircuitBreaker.Hystrix;
 using Steeltoe.Common.Discovery;
@@ -11,11 +12,13 @@ namespace FunnyQuotesUIForms.Clients
     public class WcfFunnyQuotesClient : IFunnyQuoteService
     {
         private readonly IOptionsSnapshot<FunnyQuotesConfiguration> _config;
+        private readonly ILogger<WcfFunnyQuotesClient> _logger;
         private readonly EndpointClientHandler _dicoveryAddressResolver;
 
-        public WcfFunnyQuotesClient(IDiscoveryClient discoveryClient, IOptionsSnapshot<FunnyQuotesConfiguration> config)
+        public WcfFunnyQuotesClient(IDiscoveryClient discoveryClient, IOptionsSnapshot<FunnyQuotesConfiguration> config, ILogger<WcfFunnyQuotesClient> logger)
         {
             _config = config;
+            _logger = logger;
             _dicoveryAddressResolver = new EndpointClientHandler(discoveryClient);
         }
 
@@ -31,8 +34,16 @@ namespace FunnyQuotesUIForms.Clients
 
         public string GetCookieRun()
         {
-            var channelFactory = new ChannelFactory<IFunnyQuoteService>("FunnyQuoteServiceWcf", _dicoveryAddressResolver.GetEndpointAddress("FunnyQuoteServiceWcf"));
-            return channelFactory.CreateChannel().GetQuoteAsync().Result;
+            try
+            {
+                var channelFactory = new ChannelFactory<IFunnyQuoteService>("FunnyQuoteServiceWcf", _dicoveryAddressResolver.GetEndpointAddress("FunnyQuoteServiceWcf"));
+                return channelFactory.CreateChannel().GetQuoteAsync().Result;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error connecting to backend service");
+                throw;
+            }
         }
 
         public string GetCookieFallback() => _config.Value.FailedMessage;
--- src/FunnyQuotesUIForms/Global.asax.cs
+++ src/FunnyQuotesUIForms/Global.asax.cs
@@ -18,7 +18,8 @@ using Steeltoe.Discovery.Eureka;
 using Steeltoe.Extensions.Configuration.ConfigServer;
 using Steeltoe.Extensions.Logging;
 using Steeltoe.Management.Endpoint.Health.Contributor;
-
+using Steeltoe.Management.EndpointOwinAutofac;
+using static FunnyQuotes.StartupHelper;
 
 namespace FunnyQuotesUIForms
 {
@@ -47,9 +48,10 @@ namespace FunnyQuotesUIForms
                 builder.RegisterType<DynamicConsoleLoggerProvider>().As<ILoggerProvider>().AsSelf(); // via management endpoints
                 builder.RegisterHystrixMetricsStream(config);
                 
-                builder.RegisterType<DiskSpaceContributor>().As<IHealthContributor>();
+                builder.RegisterCloudFoundryActuators(config);
+
                 builder.RegisterType<ConfigServerHealthContributor>().As<IHealthContributor>();
-                builder.RegisterType<EurekaApplicationsHealthContributor>().As<IHealthContributor>();
+                // builder.RegisterType<EurekaApplicationsHealthContributor>().As<IHealthContributor>();
                 builder.RegisterType<EurekaServerHealthContributor>().As<IHealthContributor>();
                 
                 // register 4 different implementations of IFunnyQuoteService and assign them unique names
@@ -67,7 +69,8 @@ namespace FunnyQuotesUIForms
                 var container = builder.Build();
                 container.StartDiscoveryClient(); // start eureka client and add current app into the registry
                 container.StartHystrixMetricsStream(); // start publishing hystrix stream
-                container.StartActuators(); // map routes for actuator endpoints
+                StartupHelper.StartActuators(container);
+                // container.StartActuators(); // map routes for actuator endpoints
                 _containerProvider = new ContainerProvider(container); // setup autofac WebForms integration
                 
                 logger.LogInformation(">> FunnyQuotesLegacyUI Started <<");
